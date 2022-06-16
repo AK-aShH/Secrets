9bbf7dcb5cc6bdc6255740a4b2cfac9afaf928ef
Akash Yadav <81186992+AK-aShH@users.noreply.github.com>
2022-06-16 15:40:28 +0530

    Changed Callbackur to heroku address

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ passport.deserializeUser(function(id, done){
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://secret-fjord-40878.herokuapp.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) { // gets called when the user is authenticated successfully by google
